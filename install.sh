7ac241fe0d6bdf124713e9bf4b9111105a6d79ec
Zander Work <zander@zanderwork.com>
2021-01-31 14:32:39 -0800

    fixed makepkg

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ case $os in
 						sudo pacman -Sqyu --noconfirm git base-devel
 						git clone https://aur.archlinux.org/trizen.git
 						cd trizen
-						makepkg -si
+						makepkg -si --noconfirm
 						popd 2>&1 >/dev/null
 					fi
 				fi
